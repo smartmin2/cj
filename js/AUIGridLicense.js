7b884d9bfdf2b031227c6f7d2c177032d2f1c73f
e-young <93626945+simeunyoung@users.noreply.github.com>
2022-06-29 11:39:15 +0900

    Delete AUIGridLicense.js

--- js/AUIGridLicense.js
+++
@@ -1,9 +0,0 @@
-/*!
- * AUIGrid 3.0.11 License
- * License Type : Evaluation(Trial) Version
- * Authorized Domain(or IP) : localhost,127.0.0.1
- * Expiration Date : 2022-07-23
- * www.auisoft.net
- */
- /* eslint-disable */
-AUIGridLicense = "eyJjdCI6ImF0QTAyVnlmUVdTTlNxbURKbTJpejMreHBqbjhXRXRtY2JMNmI5SkFPSGl2amtWMHhOUXZITXFjb3JBK3hDMk1NYU12RzVGN0ZueGthOHB3aWsyaEt0cTVIN2g2d0Erd0p0U2NUNEsrd2pZPSIsIml2IjoiOTI3N2Y5ZWZkZTM2OWJkNDcwYmM0MDViYzQyN2UyY2QiLCJzIjoiZmZhZWQ2OTZkOTViZDI0MCJ9";
