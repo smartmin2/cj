c4422c0719f0280c12484f686cf25f875c0cc3c0
smartmin2 <86212806+smartmin2@users.noreply.github.com>
2022-06-29 12:00:48 +0900

    Add files via upload

---
+++ AUIGrid/AUIGridLicense.js
@@ -0,0 +1,9 @@
+/*!
+ * AUIGrid 3.0.11 License
+ * License Type : Evaluation(Trial) Version
+ * Authorized Domain(or IP) : localhost,127.0.0.1,smartmin2.github.io
+ * Expiration Date : 2022-07-29
+ * www.auisoft.net
+ */
+ /* eslint-disable */
+AUIGridLicense = "eyJjdCI6InF1SjFMSEFtbEtRQldpZXRQZHZUMGl2N0VOQU1EdzRqVWE5YlpEQ2hKeFYrM3pEeWFaSHE3Y2o0bFBMUjZqbFVPcXBkbkQ5ZUFhMDVVRE5memtBaWQ2MjdNY0ZzMG0rWG5KQWp4MEhuRVRncHo4bm1LRFF0dkJnS0Y3eDRHNW1KIiwiaXYiOiIwZGE1MTdhYzRhNTY0ZDJiNDVkMzY4MzZiOTc1YjQ3MyIsInMiOiIwNzAzY2NmOTRmOTk1YzkyIn0=";
